8d4102272b1172eabc71f8d7bbc240ecf863e17a
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2023-08-18 18:47:14 -0300

    upgrade

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+const emailInput = document.querySelector('.email-input')
+
+emailInput.addEventListener('focus', emailOnFocus)
+
+function emailOnFocus() {
+    emailInput.style.backgroundColor = 'blue'
+}
+
+
+
+
